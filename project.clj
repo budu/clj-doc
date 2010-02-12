364b674d7b0c957826eeb1a76f4c7f2a48d302dd
David Santiago <david.santiago@gmail.com>
2010-02-12 16:58:47 +0800

    Changed build to Leiningen. Updated .gitignore to reflect this.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-doc "0.6.0-SNAPSHOT"
+  :description "A Clojure library to generate documentation from a namespace."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+		 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+		 [compojure "0.3.2"]]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.1.0"]])
